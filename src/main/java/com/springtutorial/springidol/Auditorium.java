88b6b4e098bd7e31815bacec812723772c2dad89
Wadim <prwhite440@gmail.com>
2016-11-24 10:30:33 +0300

    Init & destroy method for bean

---
+++ src/main/java/com/springtutorial/springidol/Auditorium.java
@@ -0,0 +1,13 @@
+package com.springtutorial.springidol;
+
+/**
+ * Created by Пользователь on 24.11.2016.
+ */
+public class Auditorium {
+    public void turnOnLights() {
+        System.out.println("Lights on");
+    }
+    public void turnOffLights() {
+        System.out.println("Lights off");
+    }
+}
